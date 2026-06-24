0788396c35b043a277f61631a968f460d0be4523
timelapse4 <redhats.cc@gmail.com>
2026-06-24 15:17:44 +0700

    Add initial build configuration for M3UProvider

---
+++ M3UProvider/build.gradle.kts
@@ -0,0 +1,15 @@
+version = 1
+
+cloudstream {
+    description = "M3U IPTV – stream any M3U playlist with group categories"
+    authors = listOf("timelapse4")
+    status = 1
+    tvTypes = listOf("Live")
+    iconUrl = "https://www.google.com/s2/favicons?domain=iptv.852851.xyz&sz=128"
+    language = "en"
+}
+
+android {
+    namespace = "com.M3UProvider"
+}
+
