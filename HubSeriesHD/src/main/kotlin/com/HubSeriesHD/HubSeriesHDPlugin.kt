5e7ab8a53cc0cb887e4213945a7283dde2cf2b37
timelapse4 <redhats.cc@gmail.com>
2026-06-24 18:57:06 +0700

    Refactor HubSeriesHDPlugin to extend BasePlugin

--- HubSeriesHD/src/main/kotlin/com/HubSeriesHD/HubSeriesHDPlugin.kt
+++ HubSeriesHD/src/main/kotlin/com/HubSeriesHD/HubSeriesHDPlugin.kt
@@ -1,12 +1,11 @@
 package com.HubSeriesHD
 
-import android.content.Context
+import com.lagradost.cloudstream3.plugins.BasePlugin
 import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
-import com.lagradost.cloudstream3.plugins.Plugin
 
 @CloudstreamPlugin
-class HubSeriesHDPlugin: Plugin() {
-    override fun load(context: Context) {
+class HubSeriesHDPlugin : BasePlugin() {
+    override fun load() {
         registerMainAPI(HubSeriesHD())
     }
 }
