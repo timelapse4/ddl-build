de2c8b135b39f2587db8f1b6c21d1fe8933a2473
timelapse4 <redhats.cc@gmail.com>
2026-06-24 17:42:49 +0700

    Add build.gradle for HubSeriesHD project

---
+++ HubSeriesHD/build.gradle
@@ -0,0 +1,10 @@
+version = 1
+
+cloudstream {
+    language = "th"
+    description = "ดูซีรีส์จาก HubSeriesHD รองรับซีรีส์เกาหลี จีน ญี่ปุ่น และอื่นๆ"
+    authors = listOf("Timelapse4")
+    status = 1
+    tvTypes = listOf("AsianDrama")
+    iconUrl = "https://hubserieshd.com/wp-content/uploads/2023/01/cropped-hubserieshd-logo.png"
+}
