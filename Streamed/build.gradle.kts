09b26e1cb30f184889ffdb0941c035a0a62cded3
timelapse4 <redhats.cc@gmail.com>
2026-07-06 09:38:52 +0700

    Add initial build.gradle.kts for Streamed extension

---
+++ Streamed/build.gradle.kts
@@ -0,0 +1,12 @@
+// Streamed extension - maintained by timelapse4
+// Originally based on work by Kraptor123
+version = 1
+
+cloudstream {
+    authors     = listOf("timelapse4")
+    language    = "en"
+    description = "Watch live sports streams via Streamed."
+    status  = 1 // will be 3 if unspecified
+    tvTypes = listOf("Live") //Movie, AnimeMovie, TvSeries, Cartoon, Anime, OVA, Torrent, Documentary, AsianDrama, Live, NSFW, Others, Music, AudioBook, CustomMedia, Audio, Podcast,
+    iconUrl = "https://t2.gstatic.com/faviconV2?client=SOCIAL&type=FAVICON&fallback_opts=TYPE,SIZE,URL&url=https://streamed.pk/&size=128"
+}
