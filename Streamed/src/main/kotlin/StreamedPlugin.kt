2c195cfeab655d9b03e77f235dfa96271ed4fd7c
timelapse4 <redhats.cc@gmail.com>
2026-07-06 10:17:16 +0700

    Refactor StreamedPlugin to extend BasePlugin

--- Streamed/src/main/kotlin/StreamedPlugin.kt
+++ Streamed/src/main/kotlin/StreamedPlugin.kt
@@ -1,19 +1,17 @@
 // Streamed extension - maintained by timelapse4
 // Originally based on work by Kraptor123
 
+import com.lagradost.cloudstream3.plugins.BasePlugin
 import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
-import com.lagradost.cloudstream3.plugins.Plugin
-import android.content.Context
 import com.timelapse4.EmbedSporty
 import com.timelapse4.EmbedStreams
 import com.timelapse4.Streamed
 
 @CloudstreamPlugin
-class StreamedPlugin: Plugin() {
-    override fun load(context: Context) {
+class StreamedPlugin : BasePlugin() {
+    override fun load() {
         registerMainAPI(Streamed())
-        registerExtractorAPI(EmbedStreams(context))
-        registerExtractorAPI(EmbedSporty(context))
+        registerExtractorAPI(EmbedStreams())
+        registerExtractorAPI(EmbedSporty())
     }
 }
-
