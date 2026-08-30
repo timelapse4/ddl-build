42038a470664a4c5dd4bc5a55b7c197ef79ff8f7
timelapse4 <redhats.cc@gmail.com>
2026-08-30 19:57:17 +0700

    Add initial build configuration for TimStreams

---
+++ TimStreams/build.gradle.kts
@@ -0,0 +1,14 @@
+version = 1
+
+cloudstream {
+    description = "TimStreams - live sports & TV channels from timstreams.st"
+    authors = listOf("timelapse4")
+    status = 1
+    tvTypes = listOf("Live")
+    iconUrl = "https://timstreams.st/favicon.ico"
+    language = "en"
+}
+
+android {
+    namespace = "com.TimStreams"
+}
