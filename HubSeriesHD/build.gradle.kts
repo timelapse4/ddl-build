886db0be104d7a993efdd39502a72b0357c6e6e1
timelapse4 <redhats.cc@gmail.com>
2026-09-02 10:43:22 +0700

    Add initial build.gradle.kts configuration
    Initialized build.gradle.kts with project configuration.

---
+++ HubSeriesHD/build.gradle.kts
@@ -0,0 +1,23 @@
+// use an integer for version numbers
+version = 1
+
+
+cloudstream {
+    // All of these properties are optional, you can safely remove them
+
+    description = "ซีรี่ย์เกาหลี ฝรั่ง จีน ซับไทย/พากย์ไทย จาก hubserieshds.com"
+    authors = listOf("timelapse4")
+
+    /**
+    * Status int as one of:
+    * 0: Down
+    * 1: Ok
+    * 2: Slow
+    * 3: Beta only
+    * */
+    status = 1
+
+    tvTypes = listOf("AsianDrama")
+
+    iconUrl = "https://hubserieshds.com/images/icon-192.png"
+}
