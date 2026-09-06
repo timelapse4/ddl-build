1309922f55e35a4120233323cacc4c793f4ab9e6
timelapse4 <redhats.cc@gmail.com>
2026-09-06 19:00:56 +0700

    Remove Context parameter from load method

--- StreamFree/src/main/kotlin/com/streamfree/StreamFreePlugin.kt
+++ StreamFree/src/main/kotlin/com/streamfree/StreamFreePlugin.kt
@@ -1,12 +1,11 @@
 package com.streamfree
 
-import android.content.Context
 import com.lagradost.cloudstream3.plugins.BasePlugin
 import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
 
 @CloudstreamPlugin
 class StreamFreePlugin : BasePlugin() {
-    override fun load(context: Context) {
+    override fun load() {
         registerMainAPI(StreamFreeProvider())
         registerExtractorAPI(StreamFreeExtractor())
     }
